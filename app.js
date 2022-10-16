17cc33ef392ae00108041cf08148fac145939108
Phyllis Wong <wong.phyllism@gmail.com>
2022-10-15 20:07:16 -0600

    fix: redundant code

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-app.listen(3000, console.log('App Listening to port 3000'));
-
 app.use(express.static(path.join(__dirname, '../www')));
 
 app.listen(3000, function() {
